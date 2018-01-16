5eb07630cd5b7bae4c22f7839d21e5b444d7e89f
CaneraX <33883965+CaneraX@users.noreply.github.com>
2018-01-16 10:55:49 +0800

    Completed

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,9 +1,20 @@
-//this is an unfinish program
-//finish up this program
-
+#include<iostream>
+using namespace std;
+int sum();
 int sum(int x,int y)
 {
 	int result;
 	result = x+y;
+	cout<<result;
+	return x+y;
 
 }
+
+ int main()
+ {
+ 	int x , y;
+ 	cin>>x;
+ 	cin>>y;
+ 	sum(x , y);
+ 	return 0;
+ }
